3b0f70047f30434652fe72ddb2df2d81fc24f269
JoaoViana1 <vianajoaoguilherme1@gmail.com>
2021-08-20 21:39:39 -0300

    Segundo commit

--- Cap-2/tcpServer.py
+++ Cap-2/tcpServer.py
@@ -2,16 +2,19 @@ import socket
 import threading
 
 bind_ip = "0.0.0.0"
-bind_port = 9999
+bind_port = 1234
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+# Primeiro passamos o endereço ip e a porta que deseja
 server.bind((bind_ip, bind_port))
 
+# Agora colocamos o servidor para escutar com o atraso máximo de conexões definidas para 5
 server.listen(5)
-
 print(f"Escutando de: {bind_ip}:{bind_port}")
 
+
+# Ele executa o recv e em seguida envar pacotes de teste para o servidor
 def handle_client(client_socket):
     request = client_socket.recv(1024)
 
@@ -23,9 +26,10 @@ def handle_client(client_socket):
 
 
 while True:
-
+    # Quando o cliente se conecta mostra os detalhes da conexão remota
     client, addr = server.accept()
     print(f"Conexão aceita de: {addr[0], addr[1]}")
 
+    # Inicia o thread para lidar com a conexão do cliente e o servidor conseguirá lidar com outra conexão de entrada
     client_handler = threading.Thread(target=handle_client, args=(client,))
     client_handler.start()
